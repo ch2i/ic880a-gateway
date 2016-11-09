9b7882a44119c18337ff807c9778a9d1de5514a0
Charles <hallard04@free.fr>
2016-11-09 12:12:14 +0100

    creation

---
+++ src/check_led/check_led.c
@@ -0,0 +1,81 @@
+// check_led.cpp
+//
+// Example program showing how to check LED on Raspberry PI ic880a shield
+// Requires bcm2835 library to be already installed
+// http://www.airspayce.com/mikem/bcm2835/
+// Use the Makefile in this directory:
+// cd src/check_led
+// make
+// sudo ./check_led
+//
+// Will blink LED on GPIO4, GPIO18, GPIO23 and GPIO24
+// RasPI Lora Gateway Board iC880A 
+// https://github.com/ch2i/iC880A-Raspberry-PI
+//
+// Contributed by Charles-Henri Hallard (hallard.me)
+//
+
+#include <bcm2835.h>
+#include <stdio.h>
+#include <signal.h>    
+
+// Blink delay (in ms)
+#define BLINK_DELAY 200
+
+int     exit_sig   = 0; // application terminates cleanly
+uint8_t LED_pins[] = {4, 18, 23, 24}; // GPIO line LED
+
+void sig_handler(int sig) {
+    if (sig==SIGQUIT || sig==SIGINT || sig==SIGTERM) {
+        exit_sig = 1;
+    }
+}
+
+int main(int argc, char **argv)
+{
+  uint8_t i;
+
+  struct sigaction sigact; 
+
+  // configure signal handling 
+  sigemptyset(&sigact.sa_mask);
+  sigact.sa_flags = 0;
+  sigact.sa_handler = sig_handler;
+  sigaction(SIGQUIT, &sigact, NULL);
+  sigaction(SIGINT, &sigact, NULL);
+  sigaction(SIGTERM, &sigact, NULL);
+
+  if (!bcm2835_init()) {
+    printf("bcm2835_init failed. Are you running as root??\n");
+
+  } else {
+
+    // Drive all LED line as output and set them High (HIGH = LED Off)
+    for ( i=0; i<sizeof(LED_pins); i++) {
+      bcm2835_gpio_fsel (LED_pins[i], BCM2835_GPIO_FSEL_OUTP );
+      bcm2835_gpio_write(LED_pins[i], HIGH );
+    }
+
+    // Loop until end
+    while (exit_sig==0) {
+      // Now try to detect all modules
+      for ( i=0; i<sizeof(LED_pins); i++) {
+        // Light ON
+        bcm2835_gpio_write(LED_pins[i], LOW);
+        delay(BLINK_DELAY);
+        // Light OFF
+        bcm2835_gpio_write(LED_pins[i], HIGH);
+        delay(BLINK_DELAY);
+      }
+    }
+  }
+
+  // all LED OFF
+  for ( i=0; i<sizeof(LED_pins); i++) {
+    bcm2835_gpio_write(LED_pins[i], HIGH );
+  }
+
+  bcm2835_close();
+}
+
+
