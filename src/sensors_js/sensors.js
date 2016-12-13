4e02e3ca97ae171ca9599a63806ce6e164be7c57
Charles <hallard04@free.fr>
2016-12-13 11:36:06 +0100

    Works only if cayenne MQTT was successfull

--- src/sensors_js/sensors.js
+++ src/sensors_js/sensors.js
@@ -1,5 +1,21 @@
-
-// npm install bme280-sensor si7021-sensor cayennejs
+/*******************************************************************************
+ * NodeJS script to send sensors data to cayenne IoT dashboard
+ * you can have a BMP280/BME280 and SI7021/HTU21D conencted to I2C bus
+ * This sample has been written by Charles-Henri Hallard (ch2i.eu)
+ *  
+ * Requires nodejs to be already installed
+ * https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+ *
+ * Requires CayenneJS bme280 and si7021 libraries
+ * npm install bme280-sensor si7021-sensor cayennejs
+ *
+ * Don't forget to put your Cayenne credential in this script, all of this 
+ * stuff can be done with the script installer install.sh of this folder
+ * installer will also make the as daemon to start/stop with the system
+ * 
+ * sudo node sensors.js
+ *
+ *******************************************************************************/
 
 const BME280 = require('bme280-sensor');
 const Si7021 = require('si7021-sensor');
@@ -13,16 +29,10 @@ const updateInterval = 60;
 
 // Initiate Cayenne MQTT API
 const cayenneClient = new Cayenne.MQTT({
-  username: "a1ced9e0-b24e-11e6-bb76-1157597ded22",
-  password: "0858f39268653283bf68bb08b165c07cd6cb1959",
-  clientId: "bd6c2ab0-bd1e-11e6-9638-53ecf337e03f"
+  username: "__CAYENNE_USER__",
+  password: "__CAYENNE_PASS__",
+  clientId: "__CAYENNE_CLID__"
 });
-
-
-cayenneClient.connect((err , mqttClient) => {
-	console.log('Cayenne connected')
-})	
-
 
 // Read BME280 sensor data, repeat
 const readBME280SensorData = () => {
@@ -43,16 +53,6 @@ const readBME280SensorData = () => {
       setTimeout(readBME280SensorData, updateInterval*1000);
     });
 };
-
-// Initialize the BME280 sensor
-//
-bme280.init()
-  .then(() => {
-    console.log('BME280 initialization succeeded');
-    readBME280SensorData();
-  })
-  .catch((err) => console.error(`BME280 initialization failed: ${err} `));
-	
 	
 const readSI7021SensorData = () => {
   si7021.readSensorData()
@@ -71,10 +71,23 @@ const readSI7021SensorData = () => {
     });
 };
 
-si7021.reset()
-  .then(() => {
-    console.log('SI7021 initialization succeeded');
-		readSI7021SensorData();
-	})
-  .catch((err) => console.error(`Si7021 reset failed: ${err} `));
-	
+cayenneClient.connect((err , mqttClient) => {
+	console.log('Cayenne connected')
+
+	// Initialize the BME280 sensor
+	bme280.init()
+		.then(() => {
+			console.log('BME280 initialization succeeded');
+			readBME280SensorData();
+		})
+		.catch((err) => console.error(`BME280 initialization failed: ${err} `));
+		
+	// Initialize the si7021 sensor
+	si7021.reset()
+		.then(() => {
+			console.log('SI7021 initialization succeeded');
+			readSI7021SensorData();
+		})
+		.catch((err) => console.error(`Si7021 reset failed: ${err} `));
+})	
+
